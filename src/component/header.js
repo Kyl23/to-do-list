0c2b562af2c27e17b8d65f31cff2e5a43bbcc75e
Kyl23 <cklkailiang@gmail.com>
2021-11-06 20:39:08 +0800

    switch to JSX style name

--- src/component/header.js
+++ src/component/header.js
@@ -1,12 +1,12 @@
 import Input from "./input";
-import Put_Button from "./put_to_do"
+import PutButton from "./put_to_do"
 import React from 'react'
 
 function Header({txt,SetTxt,SetTask}) {
     return (
         <div className="header">
             <Input txt={txt} settxt={SetTxt}/>
-            <Put_Button txt={txt} settxt={SetTxt} settask={SetTask}/>
+            <PutButton txt={txt} settxt={SetTxt} settask={SetTask}/>
         </div>
     )
 }
