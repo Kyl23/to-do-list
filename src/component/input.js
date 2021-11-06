0d7ae3cfc66e8628d48cee3169b3c969823b93e5
Kyl23 <cklkailiang@gmail.com>
2021-11-06 20:41:13 +0800

    fragment pluguseless fragment has been plugged out

--- src/component/input.js
+++ src/component/input.js
@@ -2,11 +2,9 @@ import PropTypes from 'prop-types'
 const Input = ({txt,settxt}) => {
    
     return (
-        <>
-            <input className="inpt" type="text" placeholder="新增任務" value={txt} onChange={
-                (e)=>settxt(e.target.value)
-            }/>
-        </>
+        <input className="inpt" type="text" placeholder="新增任務" value={txt} onChange={
+            (e)=>settxt(e.target.value)
+        }/>
     )
 }
 Input.prototype={
