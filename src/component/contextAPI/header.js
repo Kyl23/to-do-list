8bc347c751535e3df882cdfbdc7634105f0ec8e1
Kyl23 <cklkailiang@gmail.com>
2021-11-21 02:21:25 +0800

    fix warning

--- src/component/contextAPI/header.js
+++ src/component/contextAPI/header.js
@@ -1,6 +1,6 @@
 import Input from "./input";
 import PutButton from "./put_to_do";
-import {  useCallback, useContext, useEffect } from "react";
+import { useState, useContext, useEffect } from "react";
 import { HeadDiv } from "../../style/css_element";
 import { AppPool } from "../../Pool/AppPool";
 import { HomePgContext } from "../../context/HomePgContext";
@@ -8,39 +8,36 @@ function Header() {
   const poolValue = useContext(AppPool);
   const homeValue = useContext(HomePgContext);
   const [, setTasks] = homeValue.taskContext;
-  const [pool,] = poolValue.PoolContext;
-  const [isLegal,]=poolValue.ActiveContext;
+  const [pool] = poolValue.PoolContext;
+  const [isLegal] = poolValue.ActiveContext;
   const [id, setId] = homeValue.idContext;
-  const getTasksFromServer = useCallback(async () => {
+  const getTasksFromServer = async () => {
     const response = await fetch(
       "https://raw.githubusercontent.com/JiaAnTW/json_storage/master/react-tutorial/todolist.json",
       { method: "GET" }
-    )
-     const data=await response.text();
-     const result=await JSON.parse(data).data
+    );
+    const data = await response.text();
+    const result = await JSON.parse(data).data;
 
-     return result;
-  },[])
-  
-  const determineAction =useCallback((data) => {
-    
-    if (id === 0 && pool.length !== 0) 
-      setTasks(pool);
-    else if(isLegal){
-      setId(data.length+1)
-      setTasks(data)
-    }
-  },[id,setId,setTasks,isLegal,pool])
+    return result;
+  };
 
+  const [Holder, setHolder] = useState([]);
   useEffect(() => {
-    const getTasks=async ()=>{
-      const tasksFromServer= await getTasksFromServer();
-     
-      determineAction(tasksFromServer)
-    }
+    const getTasks = async () => {
+      const tasksFromServer = await getTasksFromServer();
+      if (id === 0 && isLegal) setHolder(tasksFromServer);
+      else if (id === 0 && pool.length !== 0) {
+        setId(pool.length + 1);
+        setTasks(pool);
+      }
+    };
     getTasks();
-  }, []);
-
+  }, [id, pool, setTasks, isLegal, setId]);
+  useEffect(() => {
+    setId(Holder.length + 1);
+    setTasks(Holder);
+  }, [Holder, setId, setTasks]);
 
   return (
     <HeadDiv>
